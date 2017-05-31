3f7eee1839aee509ebc1ae493965b3807849e1a6
Kate Latypova <lakate@yandex-team.ru>
2017-05-31 12:30:41 +0500

    правки 7 задачи

--- hw7/main.c
+++ hw7/main.c
@@ -7,137 +7,143 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define MAX_SIZE 1000
+#define MAX_SIZE 50
+#define MAX_NUMBER 30
 
-int pipes[MAX_SIZE];
+/*
+Параллельно вычисляется факториал для каждого числа из файла
+Они должны быть неотрицательными целыми числами
+Для наглядности обработчики дополнительно усыпляю на 5 секунд
+*/
 
-int amount;
 long long numbers[MAX_SIZE];
+int pipes[MAX_SIZE];
+int n_numbers = 0;
 
-void print_result() {
-    int i, result, overall = 0;
-    for (i = 0; i < amount; ++i) {
-        FILE * fd = fdopen(pipes[i], "r");
-        fscanf(fd, "%d", &result);
-        fclose(fd);
-        overall += result;
+int Factorial(long long number) {
+    if (number < 0) {
+        return -1;
+    }else if (number < 2) {
+        return 1;
     }
-    printf("Amount of prime numbers: %d\n", overall);
+    return (number*Factorial(number-1));
 }
 
-int isPrime(long long number) {
-    int i;
-    if (number % 2 == 0) {
-        return 0;
-    }
-    for (i = 3; i <= number - 1 ; ++i) {
-        if (number % i == 0) {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-void parallel_execute(int read, int write) {
+void parallel(int input, int output, int index) {
     fd_set fds;
-    struct timeval tv;
+    FD_ZERO(&fds);
+    FD_SET(input, &fds);
+
+    struct timeval t;
+    t.tv_sec = 5;
+    t.tv_usec = 0;
+
     int retval;
-
-    FD_ZERO(&fds);
-    FD_SET(read, &fds);
-
-    tv.tv_sec = 5;
-    tv.tv_usec = 0;
-
-    retval = select(4000, &fds, NULL, NULL, &tv);
+    retval = select(pipes[index] + 1, &fds, NULL, NULL, &t);
     if (retval == -1) {
-        printf("Error! Select error!\n");
-        exit(2);
+        printf("Select error!\n");
+
+        exit(1);
     }
 
-    FILE * fd;
+    FILE * f;
     long long number;
-    fd = fdopen(read, "r");
-    fscanf(fd, "%lld", &number);
-    fclose(fd);
 
-    int result;
-    result = isPrime(number);
+    f = fdopen(input, "r");
+    fscanf(f, "%lld", &number);
+    fclose(f);
 
     FD_ZERO(&fds);
-    FD_SET(write, &fds);
+    FD_SET(output, &fds);
+    retval = select(pipes[index] + 1, NULL, &fds, NULL, &t);
 
-    retval = select(4000, NULL, &fds, NULL, &tv);
     if (retval == -1) {
-        printf("Error! Select error!\n");
-        exit(2);
+        printf("Select error!\n");
+
+        exit(1);
     }
 
-    fd = fdopen(write, "w");
-    fprintf(fd, "%d\n", result);
-    fclose(fd);
+    f = fdopen(output, "w");
+    fprintf(f, "%d\n", Factorial(number));
+    fclose(f);
+
+    sleep(5);
 }
 
-void fork_to_execute(int index, long long number) {
+void fork_handler(long long number, int index) {
     int fd[2];
     pipe(fd);
+    pid_t pid;
+    pid = fork();
+    FILE *f;
 
-    pid_t childpid;
-    if((childpid = fork()) == -1) {
-        printf("Error! Can't fork!\n");
+    switch(pid) {
+        case -1:
+            printf("Fork error\n");
+            exit(1);
+        case 0:
+            parallel(fd[0], fd[1], index);
+            exit(0);
+        default:
+            f = fdopen(fd[1], "w");
+            fprintf(f, "%lld\n", number);
+            fclose(f);
+            pipes[index] = fd[0];
+    }
+}
+
+void get_numbers(char * file) {
+    FILE * f = fopen(file, "r");
+
+    if (f == NULL) {
+        printf("Can't open file %s\n", file);
+
         exit(1);
     }
 
-    if(childpid == 0) {
-        parallel_execute(fd[0], fd[1]);
-        exit(0);
-    } else {
-        FILE *fp = fdopen(fd[1], "w");
-        fprintf(fp, "%lld\n", number);
-        fclose(fp);
+    while (fscanf(f, "%lld", &numbers[n_numbers]) == 1) {
+        if ((numbers[n_numbers])> MAX_NUMBER){
+            printf("Too big number: %lld", numbers[n_numbers]);
 
-        pipes[index] = fd[0];
+            exit(1);
+        }
+
+        n_numbers++;
     }
+
+    fclose(f);
 }
 
-void read_file(char * filename) {
-    amount = 0;
-    FILE * fd = fopen(filename, "r");
-    if (fd == NULL) {
-        printf("Error! File don't exist!\n");
-        exit(1);
-    }
-    while (fscanf(fd, "%lld", &numbers[amount]) == 1) {
-        ++amount;
-    }
-    fclose(fd);
-}
-
-void check_all_numbers() {
-    int i;
-    for (i = 0; i < amount; ++i) {
-        fork_to_execute(i, numbers[i]);
-    }
-}
-
-void wait_all_childs() {
-    int count = amount;
-    while (count) {
-        wait(NULL);
-        --count;
-    }
-}
 
 int main(int argc, char * argv[]) {
     if (argc != 2) {
-        printf("Error! You entered wrong parameters!\n");
+        printf("Wrong number of arguments\n");
+
         return 1;
     }
+    get_numbers(argv[1]);
 
-    read_file(argv[1]);
-    check_all_numbers();
-    wait_all_childs();
-    print_result();
+    int i;
+    for (i = 0; i < n_numbers; ++i) {
+        fork_handler(numbers[i], i);
+    }
+
+    int active = n_numbers;
+    while (active) {
+        wait(NULL);
+        active--;
+    }
+
+    int fact;
+    long long result;
+
+    for (i = 0; i < n_numbers; ++i) {
+        FILE * f = fdopen(pipes[i], "r");
+        fscanf(f, "%d", &result);
+        fclose(f);
+
+        printf ("Fact(%lld) = %lld\n", numbers[i], result);
+    }
 
     return 0;
 }
